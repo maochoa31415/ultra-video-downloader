9f19155716c986ae519e4cfd083158c16fdd8e2b
maochoa31415 <maochoa31415@gmail.com>
2024-09-13 15:05:50 -0400

    - Generate paths for APIs, by using environment variables. - UI fallback if the server not responding.

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,17 @@
+const {
+  VITE_API_PATH_GET_TOKEN,
+  VITE_API_PATH_INFO,
+  VITE_API_PATH_DOWNLOAD,
+  VITE_API_PATH_CANCEL_DOWNLOAD,
+} = import.meta.env
+
+export const API_PORT = import.meta.env.VITE_API_PORT
+export const ENV_DEVELOP = import.meta.env.DEV
+export const API_URL = ENV_DEVELOP
+  ? `http://localhost:${API_PORT}`
+  : `${window.location.origin}`
+export const API_TOKEN = `${API_URL}${VITE_API_PATH_GET_TOKEN}`
+export const API_INFO = `${API_URL}${VITE_API_PATH_INFO}`
+export const API_DOWNLOAD = `${API_URL}${VITE_API_PATH_DOWNLOAD}`
+export const API_CANCEL_DOWNLOAD = `${API_URL}${VITE_API_PATH_CANCEL_DOWNLOAD}`
 export const INDEX_NOT_FOUND = -1;
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,12 +1,26 @@
 <template>
   <v-app>
-    <v-main>
+    <v-main v-if="appIsReady">
       <router-view />
+    </v-main>
+    <v-main v-else>
+      <v-container class="d-flex" height="100vh" max-width="520px">
+        <div class="text-center ma-auto">
+          <v-icon icon="mdi-antenna" class="text-h1" color="error" />
+          <div class="text-h3 font-weight-light user-select-none">
+            Ups, the server is not responding.
+          </div>
+        </div>
+      </v-container>
     </v-main>
     <loading-status />
   </v-app>
 </template>
 
 <script lang="ts" setup>
-  //
+  import { storeToRefs } from 'pinia'
+  import { useAppStore } from '@/stores/app'
+
+  const store = useAppStore()
+  const { appIsReady } = storeToRefs(store)
 </script>
